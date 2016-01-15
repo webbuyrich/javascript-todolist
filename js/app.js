dd9cf6bac5157975e6a8a73d230def9d55361cb9
Richard Peterson <webbuyrich@gmail.com>
2016-01-15 10:26:09 -0600

    added template files. set up js comments

---
+++ js/app.js
@@ -0,0 +1,58 @@
+//Problem: User interation doesn't provide desired results.
+//Solution: Add interactivity so the user can manage daily tasks.
+
+//Add New task
+	//When the button is pressed
+	//Create a new list item with the text from #new-task:
+		//input(checkbox)
+		//label
+		//input(text)
+		//button.edit
+		//button.delete
+		//Each element will need to be modified and appended
+
+
+
+
+
+
+
+//Edit an existing task
+	//When the Edit button is pressed
+	//If the parent has the class .editMode
+		//Switch from .editMode
+		//label text become the input's value
+	//else
+		//Switch to .editMode
+		//input value becomes the label's text
+
+	//Togle .editMode on the parent
+
+
+
+
+
+
+//Delete an existing task
+	//When the Delete button is pressed
+		//Remove the parent list item from the ul
+	
+
+
+
+
+
+
+//Mark a task as complete
+	//When the checkbox is checked
+		//Append the task list item to the #completed-tasks
+
+
+
+
+
+
+
+//Mark a task as incomplete
+	//When checkbox is unchecked
+		//Append to incomplete tasks
