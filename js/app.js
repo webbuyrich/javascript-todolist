73ffb05f9bfb0f7a959cad3671f463ec7780ea1d
Richard Peterson <webbuyrich@gmail.com>
2016-01-15 10:37:04 -0600

    declared starting variables

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 //Problem: User interation doesn't provide desired results.
 //Solution: Add interactivity so the user can manage daily tasks.
 
+
+var taskInput = document.getElementById('new-task');;//new-task
+var addButton = document.getElementsByTagName('button')[0];//first button
+var incompleteTasksHolder = document.getElementById('incomplete-tasks');; //incomplete-tasks
+var completedTasksHolder = document.getElementById('completed-tasks');;//completed-tasks
+
+
+
 //Add New task
+var addTask = function(){
 	//When the button is pressed
 	//Create a new list item with the text from #new-task:
 		//input(checkbox)
@@ -15,9 +24,11 @@
 
 
 
+}
 
 
 //Edit an existing task
+var editTask = function(){
 	//When the Edit button is pressed
 	//If the parent has the class .editMode
 		//Switch from .editMode
@@ -28,14 +39,38 @@
 
 	//Togle .editMode on the parent
 
+}
+
 
 
 
 
 
 //Delete an existing task
+var deleteTask = function(){
 	//When the Delete button is pressed
 		//Remove the parent list item from the ul
+
+
+}
+
+
+
+//Mark a task as complete
+var completeTask = function(){
+	//When the checkbox is checked
+		//Append the task list item to the #completed-tasks
+}
+
+
+
+
+
+//Mark a task as incomplete
+var incompleteTask = function(){
+	//When checkbox is unchecked
+		//Append to incomplete tasks
+}
 	
 
 
@@ -43,9 +78,6 @@
 
 
 
-//Mark a task as complete
-	//When the checkbox is checked
-		//Append the task list item to the #completed-tasks
 
 
 
@@ -53,6 +85,3 @@
 
 
 
-//Mark a task as incomplete
-	//When checkbox is unchecked
-		//Append to incomplete tasks
